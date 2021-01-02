3690e8514c392e8c21314dfa04f4afbb879baa2f
Soldy <4786022+Soldy@users.noreply.github.com>
2021-01-02 12:02:29 +0000

    typed js type hint
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,165 +1,312 @@
-'use strict'
+'use strict';
 const w = window;
 const q = document;
 const d = q.getElementById;
 const c = q.getElementsByClassName;
 const n = q.getElementsByName;
 const t = q.getElementsByTagName;
+
+/*
+ * @param {string}
+ * return {string}
+ *
+*/
 const dv = function(i){
     return d(i).value;
-}
+};
+/*
+ * @param {string}
+ * return {string}
+ *
+*/
 const di = function(i){
     return d(i).innerHTML;
-}
+};
+/*
+ * @param {string}
+ * @param {string}
+ * return {string}
+ *
+*/
 const ds = function(i, s){
     return d(i).style[s];
-}
+};
+/*
+ * @param {string}
+ * @return {integer}
+*/
 const dl = function(i){
     if(typeof d(i) === 'undefined')
         return 0;
     return 1;
-}
+};
+/*
+ * @param {string}
+ * @param {string}
+ * @return {boolean}
+*/
 const dV = function(i, v){
     return d(i).value = v;
-}
+};
+/*
+ * @param {string}
+ * @param {string}
+ * @return {boolean}
+*/
 const dI = function(i, v){
     return d(i).innerHTML = v;
-}
+};
+/*
+ * @param {string}
+ * @param {string}
+ * @return {boolean}
+*/
 const dS = function(i, s, v){
     return d(i).style[s] = v;
-}
+};
 
 
+/* 
+ * @param {string}
+ * return {string}
+ *
+*/
 const cv = function(i){
     return c(i)[0].value;
-}
+};
+/*
+ * @param {string}
+ * return {string}
+ *
+*/
 const ci = function(i){
     return c(i)[0].innerHTML;
-}
+};
+/*
+ * @param {string}
+ * @param {string}
+ * return {string}
+ *
+*/
 const cs = function(i, s){
     return c(i)[0].style[s];
-}
+};
+/*
+ * @param {string}
+ * @return {integer}
+*/
 const cl = function(i){
     return c(i).length;
-}
+};
+/*
+ * @param {string}
+ * @param {string}
+ * @return {boolean}
+*/
 const cV = function(i, v){
     if(typeof c(i) === 'undefined')
         return false;
     for (let u = 0 ; cl(i) > u ; u++)
         c(i)[u].value = v;
     return true;
-}
+};
+/*
+ * @param {string}
+ * @param {string}
+ * @return {boolean}
+*/
 const cI = function(i, v){
     if(typeof c(i) === 'undefined')
         return false;
     for (let u = 0 ; cl(i) > u ; u++)
         c(i)[u].innerHTML = v;
     return true;
-}
+};
+/*
+ * @param {string}
+ * @param {string}
+ * @return {boolean}
+*/
 const cS = function(i, s, v){
     if(typeof c(i) === 'undefined')
         return false;
     for (let u = 0 ; cl(i) > u ; u++)
-        c(i)[[u].style[s] = v;
+        c(i)[u].style[s] = v;
     return true;
-}
+};
 
+
+
+/* 
+ * @param {string}
+ * return {string}
+ *
+*/
 const nv = function(i){
     return n(i)[0].value;
-}
+};
+/*
+ * @param {string}
+ * return {string}
+ *
+*/
 const ni = function(i){
     return n(i)[0].innerHTML;
-}
+};
+/*
+ * @param {string}
+ * @param {string}
+ * return {string}
+ *
+*/
 const ns = function(i, s){
     return n(i)[0].style[s];
-}
+};
+/*
+ * @param {string}
+ * @return {integer}
+*/
 const nl = function(i){
     return n(i).length;
-}
+};
+/*
+ * @param {string}
+ * @param {string}
+ * @return {boolean}
+*/
 const nV = function(i, v){
     if(typeof n(i) === 'undefined')
         return false;
     for (let u = 0 ; nl(i) > u ; u++)
         n(i)[u].value = v;
     return true;
-}
+};
+/*
+ * @param {string}
+ * @param {string}
+ * @return {boolean}
+*/
 const nI = function(i, v){
     if(typeof n(i) === 'undefined')
         return false;
     for (let u = 0 ; nl(i) > u ; u++)
         n(i)[u].innerHTML = v;
     return true;
-}
+};
+/*
+ * @param {string}
+ * @param {string}
+ * @return {boolean}
+*/
 const nS = function(i, s, v){
     if(typeof n(i) === 'undefined')
         return false;
     for (let u = 0 ; nl(i) > u ; u++)
-        n(i)[[u].style[s] = v;
+        n(i)[u].style[s] = v;
     return true;
-}
+};
+
+
+
+
+/*
+ * @param {string}
+ * return {string}
+ *
+*/
 const tv = function(i){
     return t(i)[0].value;
-}
+};
+/*
+ * @param {string}
+ * return {string}
+ *
+*/
 const ti = function(i){
     return t(i)[0].innerHTML;
-}
+};
+/*
+ * @param {string}
+ * @param {string}
+ * return {string}
+ *
+*/
 const ts = function(i, s){
     return t(i)[0].style[s];
-}
+};
+/*
+ * @param {string}
+ * @return {integer}
+*/
 const tl = function(i){
     return t(i).length;
-}
+};
+/*
+ * @param {string}
+ * @param {string}
+ * @return {boolean}
+*/
 const tV = function(i, v){
     if(typeof t(i) === 'undefined')
         return false;
     for (let u = 0 ; tl(i) > u ; u++)
         t(i)[u].value = v;
     return true;
-}
+};
+/*
+ * @param {string}
+ * @param {string}
+ * @return {boolean}
+*/
 const tI = function(i, v){
-    if(typeof n(i) === 'undefined')
+    if(typeof t(i) === 'undefined')
         return false;
     for (let u = 0 ; tl(i) > u ; u++)
         t(i)[u].innerHTML = v;
     return true;
-}
+};
+/*
+ * @param {string}
+ * @param {string}
+ * @return {boolean}
+*/
 const tS = function(i, s, v){
     if(typeof t(i) === 'undefined')
         return false;
     for (let u = 0 ; tl(i) > u ; u++)
-        t(i)[[u].style[s] = v;
+        t(i)[u].style[s] = v;
     return true;
-}
+};
 
 
 // inbuilt object extra
 String.prototype.T = function () {
     return this.toString();
-}
+};
 String.prototype.F = function () {
     return parseFloat(this);
-}
+};
 String.prototype.I = function () {
     return parseInt(this);
-}
+};
 String.prototype.P = function () {
     return parseInt(this).toString();
-}
+};
 String.prototype.R = function (a, b) {
     return this.replace(a, b);
-}
+};
 Number.prototype.T = function () {
     return this.toString();
-}
+};
 Number.prototype.F = function () {
     return parseFloat(this);
-}
+};
 Number.prototype.I = function () {
     return parseInt(this);
-}
+};
 Number.prototype.P = function () {
     return  parseInt(this).toString();
-}
+};
 
 
 
